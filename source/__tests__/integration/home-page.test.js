86aee8b1563df8028e2dd6fe7390652ab82f692c
Joshua Greben <jgreben@stanford.edu>
2018-10-03 16:35:14 -0700

    Adds basic home page integration test

---
+++ source/__tests__/integration/home-page.test.js
@@ -0,0 +1,16 @@
+describe('Sinopia Profile Editor Homepage', () => {
+
+    beforeAll(async () => {
+        await page.goto('http://127.0.0.1:8000')
+    })
+
+    it('has a link to the create new profile page', async () => {
+        const link = await page.$eval('.new-profile', e => e.getAttribute('href'))
+        expect(link).toMatch(/#\/profile\/create/)
+    })
+
+    it('loads our angular app', async () => {
+        const app = await page.$eval('html', e => e.getAttribute('ng-app'))
+        expect(app).toMatch(/locApp/)
+    })
+});
