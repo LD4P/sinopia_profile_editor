7d3cdc4c053f16842e211c14431dbc896ab6e40b
Joshua Greben <jgreben@stanford.edu>
2018-10-31 14:36:09 -0700

    Tests importing and editing an existing profile
    strip newlines when comparing text content
    
    Adds TODOs to finish up tests
    
    Make selectors less brittle

--- source/__tests__/integration/profile-create.test.js
+++ source/__tests__/integration/profile-create.test.js
@@ -3,14 +3,14 @@ const path = require('path')
 describe('Sinopia Profile Editor Create Page', () => {
   describe('Upload dialog not showing', () => {
     beforeAll(async () => {
-      await page.goto('http://127.0.0.1:8000/#/profile/create')
+      await page.goto('http://127.0.0.1:8000/#/profile/create/')
     })
 
     it('text on page', async () => {
-      await expect_regex_in_selector_textContent('h3.portlet-title', /\s*Profile\s*$/)
+      await expect_regex_in_selector_textContent('h3.portlet-title', /Create a new Profile\s*\*Required Fields$/)
     })
     it('profile metadata span', async() => {
-      await expect_value_in_selector_textContent('h4.profile-header > span#profileBanner.accordion-toggle > span.ng-scope', 'Profile')
+      await expect_regex_in_selector_textContent('h4.profile-header > span#profileBanner.accordion-toggle > span.ng-scope', 'Profile')
     });
 
     describe('header', () => {
@@ -27,26 +27,6 @@ describe('Sinopia Profile Editor Create Page', () => {
       await expect_value_in_selector_textContent('div.sinopia-footer > a', 'Linked Data for Production 2 (LD4P2)')
     })
   })
-
-  describe('Upload dialog showing', () => {
-    it('can upload a local Profile', async () => {
-      page
-        .goto('http://127.0.0.1:8000/#/profile/create/true')
-        .then(async () => await page.waitForNavigation({waitUntil: 'load'}), 10000)
-        .catch(error => console.log(`promise error for loading create page with import dialog: ${error}`))
-      const bf_item_location = path.join(__dirname, "..", "__fixtures__", 'item.json')
-
-      await expect(page).toUploadFile(
-        'input[type="file"]',
-        bf_item_location,
-      )
-      await page.waitForSelector('span[popover-title="Profile ID: profile:bf2:Item"]') // Waits for the Profile to load
-
-      await expect_regex_in_selector_textContent('span[popover-title="Profile ID: profile:bf2:Item"]', /\s*BIBFRAME 2.0 Item\s*/)
-      await expect_regex_in_selector_textContent('span[popover-title="Resource ID: profile:bf2:Item"]', /\s*BIBFRAME 2.0 Item\s*/)
-      await expect_regex_in_selector_textContent('span[popover-title="Resource ID: profile:bf2:Item:Access"]', /\s*Lending or Access Policy\s*/)
-    }, 15000)
-  }, 15000)
 })
 
 async function expect_sel_to_exist(sel) {
@@ -57,7 +37,8 @@ async function expect_sel_to_exist(sel) {
 async function expect_regex_in_selector_textContent(sel, regex) {
   await page.waitForSelector(sel)
   const sel_text = await page.$eval(sel, e => e.textContent)
-  expect(sel_text).toMatch(regex)
+  const strippedTextContent = sel_text.replace(/\n+/g,' ').trim()
+  expect(strippedTextContent).toMatch(regex)
 }
 async function expect_value_in_selector_textContent(sel, value) {
   await page.waitForSelector(sel)
