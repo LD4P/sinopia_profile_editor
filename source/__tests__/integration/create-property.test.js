0ef8e9b1e56500e83a6d9517308b28deb44ce72c
Jeremy Nelson <jermnelson@gmail.com>
2018-11-15 10:39:13 -0800

    fixed syntax error

--- source/__tests__/integration/create-property.test.js
+++ source/__tests__/integration/create-property.test.js
@@ -1,6 +1,6 @@
 // Copyright 2018 Stanford University see Apache2.txt for license
 
-**/describe('Create profile has properties for resource and templates for property', () => {
+describe('Create profile has properties for resource and templates for property', () => {
   beforeAll(async () => {
     await page.goto('http://127.0.0.1:8000/#/profile/create/')
     page
