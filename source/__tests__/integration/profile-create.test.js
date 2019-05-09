9d46914d45ed22a409d300869ff914f2da23f537
Naomi Dushay <ndushay@stanford.edu>
2019-05-09 00:31:28 -0700

    fix profile-create test false positives

--- source/__tests__/integration/profile-create.test.js
+++ source/__tests__/integration/profile-create.test.js
@@ -1,36 +1,32 @@
 // Copyright 2018 Stanford University see Apache2.txt for license
 
 describe('Sinopia Profile Editor Create Page', () => {
-  describe('Upload dialog not showing', () => {
-    beforeAll(async () => {
-      await page.goto('http://127.0.0.1:8000/#/profile/create/')
-    })
+  beforeAll(async () => {
+    return await page.goto('http://127.0.0.1:8000/#/profile/create/')
+  })
 
-    it('text on page', async () => {
-      await expect_regex_in_selector_textContent('h3.portlet-title', /Create a new Profile\s*\*Required Fields$/)
-    })
-    it('profile metadata span', async() => {
-      await expect_regex_in_selector_textContent('h4.profile-header > span#profileBanner.accordion-toggle > span.ng-scope', 'Profile')
-    });
+  it('text on page', async () => {
+    expect.assertions(1)
+    await expect_regex_in_selector_textContent('h3.portlet-title', /Create a new Profile\s*\*Required Fields$/)
+  })
+  it('profile metadata span', async() => {
+    expect.assertions(1)
+    await expect_regex_in_selector_textContent('h4.profile-header > span#profileBanner.accordion-toggle > span.ng-scope', 'Profile')
+  })
 
-    describe('header', () => {
-      it('links', async () => {
-        await expect_value_in_selector_textContent('div.sinopia-headerlinks > a:nth-child(1)', 'Linked Data Editor')
-        await expect_value_in_selector_textContent('div.sinopia-headerlinks > a:nth-child(2)', 'Help and Resources')
-      })
-    })
-    it('footer', async () => {
-      await expect(page).toMatch('funded by the Andrew W. Mellon Foundation')
-      await expect_value_in_selector_textContent('div.sinopia-footer > a:nth-child(2)', 'Linked Data for Production 2 (LD4P2)')
-    })
+  it('header links', async () => {
+    expect.assertions(2)
+    await expect_value_in_selector_textContent('div.sinopia-headerlinks > a:nth-child(1)', 'Linked Data Editor')
+    await expect_value_in_selector_textContent('div.sinopia-headerlinks > a:nth-child(2)', 'Help and Resources')
+  })
+
+  it('footer', async () => {
+    expect.assertions(2)
+    await expect(page).toMatch('funded by the Andrew W. Mellon Foundation')
+    await expect_value_in_selector_textContent('div.sinopia-footer > a:nth-child(2)', 'Linked Data for Production 2 (LD4P2)')
   })
 })
 
-async function expect_sel_to_exist(sel) {
-  await page.waitForSelector(sel)
-  const sel_text = !!(await page.$(sel))
-  expect(sel_text).toEqual(true)
-}
 async function expect_regex_in_selector_textContent(sel, regex) {
   await page.waitForSelector(sel)
   const sel_text = await page.$eval(sel, e => e.textContent)
