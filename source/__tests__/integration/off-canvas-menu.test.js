8bfec0d5db8698300b8514e622d56fb739c1c628
Naomi Dushay <ndushay@stanford.edu>
2019-05-09 00:33:20 -0700

    fix off-canvas-menu test false positives

--- source/__tests__/integration/off-canvas-menu.test.js
+++ source/__tests__/integration/off-canvas-menu.test.js
@@ -2,10 +2,11 @@
 
 describe('Off-canvas Help Menu', () => {
   beforeAll(async () => {
-    await page.goto('http://127.0.0.1:8000/#/profile/create/')
+    return await page.goto('http://127.0.0.1:8000/#/profile/create/')
   })
 
   it('clicking on the Help and Resource menu displays off-canvas menu ', async () => {
+    expect.assertions(1)
     await page.click("div.sinopia-headertext > div > a:nth-child(2)")
     const off_canvas_menu = await page.$eval("#offCanvasMenu", e => e.style.width)
     expect(off_canvas_menu).toBe("300px")
