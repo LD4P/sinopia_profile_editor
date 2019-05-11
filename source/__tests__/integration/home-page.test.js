dbfb39b74d0f4a26dca6cea218570731d49c7621
Naomi Dushay <ndushay@stanford.edu>
2019-05-11 16:13:22 -0700

    fix home-page test false positives

--- source/__tests__/integration/home-page.test.js
+++ source/__tests__/integration/home-page.test.js
@@ -3,62 +3,63 @@
 describe('Sinopia Profile Editor Homepage', () => {
 
   beforeAll(async () => {
-    await page.goto('http://127.0.0.1:8000')
-  })
-
-  afterAll(async () =>{
-    await page.waitFor(1000)
+    return await page.goto('http://127.0.0.1:8000')
   })
 
   it('redirects to profile/sinopia', async () => {
+    expect.assertions(1)
     const new_url = await page.evaluate(() => window.location.href)
     expect(new_url).toBe('http://127.0.0.1:8000/#/profile/sinopia')
   })
 
   it('website title', async () => {
+    expect.assertions(1)
     await expect_value_in_selector_textContent('title', 'Sinopia Profile Editor')
   })
 
   describe('header', () => {
     it('title', async() => {
+      expect.assertions(2)
       await expect_value_in_selector_textContent('h2.sinopia-subtitle > a:nth-child(1)', 'Sinopia')
       await expect_value_in_selector_textContent('h1.sinopia-title', 'Profile Editor')
     });
     it('links', async () => {
+      expect.assertions(2)
       await expect_value_in_selector_textContent('div.sinopia-headerlinks > a:nth-child(1)', 'Linked Data Editor')
       await expect_value_in_selector_textContent('div.sinopia-headerlinks > a:nth-child(2)', 'Help and Resources')
     })
   })
 
   it('text on page', async () => {
+    expect.assertions(1)
     await expect_regex_in_selector_textContent('h3.new-profile-header', /\s*Create a new Profile or Import one from your files\s*$/)
   })
 
   it('link to create new profile page', async () => {
+    expect.assertions(1)
     await expect_regex_in_selector_textContent('a.new-profile[href="#/profile/create/"]', /\s*Create new Profile\s*/)
   })
 
   it('Import button redirects to profile/create/true', async () => {
+    expect.assertions(2)
     const sel = 'a.btn.import-export[ng-click="showImport()"]'
-    page
-      .waitForSelector(sel)
-      .then(async () => await page.click(sel))
-      .catch(error => console.log(`promise error for clicking import button: ${error}`))
-    await page.waitForNavigation({waitUntil: 'load'})
+    await expect(page).toClick(sel)
+    await page.waitFor(500) // shameless green: it needs more time here; not sure what to wait for
     const new_url = await page.evaluate(() => window.location.href)
     expect(new_url).toBe('http://127.0.0.1:8000/#/profile/create/true')
   })
 
   it('footer', async () => {
+    expect.assertions(3)
     await expect(page).toMatch('funded by the Andrew W. Mellon Foundation')
     await expect_value_in_selector_textContent('div.sinopia-footer > a:nth-child(2)', 'Linked Data for Production 2 (LD4P2)')
     await expect_value_in_selector_textContent('div.sinopia-footer > a:nth-child(3)', 'Creative Commons CC0 1.0 Universal Public Domain Dedication')
   })
 
   it('loads our angular app', async () => {
-    expect_sel_to_exist('html[ng-app="locApp"]')
+    expect.assertions(1)
+    await expect_sel_to_exist('html[ng-app="locApp"]')
   })
-
 })
 
 async function expect_sel_to_exist(sel) {
