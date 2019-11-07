69252563de80ed4a35cd0cfa7a3adca3d0982503
Joshua Greben <jgreben@stanford.edu>
2019-11-07 09:25:04 -0800

    Remove dataTYpeLabel, dataTypeLabelHint and remark from generated valueDataType when URI is entered manually

--- source/assets/js/modules/profile/services/profileHandler.service.js
+++ source/assets/js/modules/profile/services/profileHandler.service.js
@@ -15,7 +15,8 @@ angular.module('locApp.modules.profile.services')
         var resAttributes = ["id","resourceURI","resourceLabel","propertyTemplates","author","date","remark","schema","adherence","source"];
         var propAttributes = ["propertyURI","propertyLabel","mandatory","repeatable","type","valueConstraint","remark", "resourceTemplates"];
         var consAttributes = ["valueLanguage","languageURI","languageLabel","valueDataType","valueTemplateRefs","useValuesFrom","editable","remark", "repeatable", "defaultURI", "defaultLiteral", "defaults", "validatePattern"];
-        var dataAttributes = ["dataTypeURI","dataTypeLabel","dataTypeLabelHint","remark"];
+        // var dataAttributes = ["dataTypeURI","dataTypeLabel","dataTypeLabelHint","remark"];
+        var dataAttributes = ["dataTypeURI"];
 
         var RESOURCE_TEMPLATE = "resourceTemplates";
         var PROPERTY_TEMPLATE = "propertyTemplates";
